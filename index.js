c44a9b8adf6c3760ab3f15311c88d1504b159e56
Orlin M Bozhinov <om@soundsapiens.com>
2016-02-03 21:39:55 +0200

    babel changes a lot

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var modify = require("modify-babel-preset");
 
-module.exports = modify("es2015", {
+module.exports = modify("babel-preset-es2015", {
   "transform-es2015-modules-commonjs": { "loose": true },
   "transform-regenerator": { "async": false, "asyncGenerators": false }
 });
