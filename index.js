f86a3d07c382d2d2507fc3b67b5d78a22819c07b
Orlin M Bozhinov <om@soundsapiens.com>
2016-02-03 21:22:37 +0200

    bring back a transformation plugin + config that was overlooked

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var modify = require("modify-babel-preset");
 
 module.exports = modify("es2015", {
-  "transform-es2015-modules-commonjs": { "loose": true }
+  "transform-es2015-modules-commonjs": { "loose": true },
+  "transform-regenerator": { "async": false, "asyncGenerators": false }
 });
